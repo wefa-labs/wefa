2534ea39fb93c18d65aabc412f623aa6e3afde60
Afo <obaone01@gmail.com>
2023-11-09 19:22:49 -0800

    removed uneeded files

--- packages/api/src/routes/goods.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, Router } from "express";
-
-export const goodsRouter = Router();
-
-// Route to post products to Square API
-goodsRouter.post("/upsert", async function (req: Request, res: Response) {
-  req.session.destroy(() => res.status(200).send(true));
-});
